bc62b97adaede4ece943c7fe5067c10f1d93069a
jobmath <math_silva_rodrigues@outlook.com>
2024-06-19 21:45:01 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,13 @@
+
+valor1 = int (input("Digite o primeiro valor : " ))
+valor2 = int (input("Digite o segundo 2 valor : " ))
+soma = valor1 + valor2
+subtracao = valor1 - valor2
+multiplicacao = valor1 * valor2
+divisao = valor1 / valor2
+
+
+print("A Soma desses valores é : " ,  soma)
+print("A subtracão desses valores é : " ,  subtracao)
+print("A multiplicacão desses valores é : " ,  multiplicacao)
+print("A divisão desses valores é : " ,  divisao)
